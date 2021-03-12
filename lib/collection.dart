aa5f7862225e71babef9b307241de19681641834
MichalKrzyzanowski <c00240696@itcarlow.ie>
2021-03-12 23:08:01 +0000

    cards are now dynamically created based on data from a list

--- lib/collection.dart
+++ lib/collection.dart
@@ -7,6 +7,10 @@ class CollectionPage extends StatefulWidget {
 }
 
 class _CollectionPageState extends State<CollectionPage> {
+  List<ArtifactData> data = [
+    ArtifactData("assets/images/m1911.png", "M1911 Pistol\nDigital Model")
+  ];
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -28,12 +32,12 @@ class _CollectionPageState extends State<CollectionPage> {
             Padding(
               padding: const EdgeInsets.fromLTRB(5.0, 10.0, 5.0, 10.0),
               child: Row(
-                children: [
-                  ArtifactCard(
-                    imagePath: "assets/images/m1911.png",
-                    description: "M1911 Pistol\nDigital Model",
-                  )
-                ],
+                children: data.map((data) {
+                  return ArtifactCard(
+                    imagePath: data.imagePath,
+                    description: data.desc,
+                  );
+                }).toList(),
               ),
             ),
           ],
