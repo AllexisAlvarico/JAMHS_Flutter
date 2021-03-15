35ce12871caa984999938f1f2f5d670c80cb0e47
Joshua Dunne <c00241588@itcarlow.ie>
2021-03-15 14:06:49 +0000

    Added first text block and Exhibit  image with rounded corners.

--- lib/aboutus.dart
+++ lib/aboutus.dart
@@ -27,15 +27,52 @@ class _AboutUsPageState extends State<AboutUsPage> {
                   ),
                 ),
                 Padding(
-                  padding: EdgeInsets.all(10.0),
-                  child: Container(
-                    height: MediaQuery.of(context).size.height,
-                    width: MediaQuery.of(context).size.width * 0.8,
-                    decoration: BoxDecoration(
-                      borderRadius: BorderRadius.circular(30),
-                      color: Colors.white,
-                    ),
+                  padding: EdgeInsets.all(
+                    MediaQuery.of(context).size.width / 30.0,
                   ),
+                  child: Container(
+                      height: MediaQuery.of(context).size.height,
+                      decoration: BoxDecoration(
+                        borderRadius: BorderRadius.circular(30),
+                        color: Colors.white,
+                      ),
+                      child: ListView(
+                          physics: NeverScrollableScrollPhysics(),
+                          children: [
+                            Padding(
+                                padding: EdgeInsets.all(5.0),
+                                child: Text(
+                                  "About Us",
+                                  style: TextStyle(
+                                      color: Colors.black,
+                                      fontSize: 32,
+                                      fontWeight: FontWeight.bold),
+                                  textAlign: TextAlign.center,
+                                )),
+                            Padding(
+                                padding: EdgeInsets.all(5.0),
+                                child: Text(
+                                  "The purpose of the Jewish American Military " +
+                                      "Historical Society is to preserve Jewish " +
+                                      "American military history, interpret the " +
+                                      "impact of Jews to military and civil " +
+                                      "service, and educating the public to these " +
+                                      "contributions.",
+                                  style: TextStyle(
+                                      color: Colors.black,
+                                      fontSize: 18,
+                                      fontWeight: FontWeight.bold),
+                                  textAlign: TextAlign.center,
+                                )),
+                            Padding(
+                                padding: EdgeInsets.all(25.0),
+                                child: ClipRRect(
+                                  borderRadius: BorderRadius.circular(25.0),
+                                  child: Image.asset(
+                                      "assets/images/exbibit.png",
+                                      fit: BoxFit.cover),
+                                ))
+                          ])),
                 )
               ],
             )));
