f003d56801c77d23d9edefff340ca27e655b7136
Joshua Dunne <c00241588@itcarlow.ie>
2021-03-15 12:40:02 +0000

    About Us Page now has layout similar to previous, and can be scrolled.

--- lib/aboutus.dart
+++ lib/aboutus.dart
@@ -11,12 +11,33 @@ class _AboutUsPageState extends State<AboutUsPage> {
     return Scaffold(
         backgroundColor: Color(0xff243C6C),
         body: Align(
-          alignment: Alignment.center,
-          child: Text(
-            "About us Page",
-            style: TextStyle(
-                color: Colors.white, fontSize: 26, fontWeight: FontWeight.bold),
-          ),
-        ));
+            alignment: Alignment.center,
+            child: ListView(
+              children: <Widget>[
+                Padding(
+                  padding: EdgeInsets.all(10.0),
+                  child: Container(
+                    height: 200,
+                    width: 200,
+                    decoration: BoxDecoration(
+                      image: DecorationImage(
+                          image: AssetImage("assets/images/homelogo.png"),
+                          fit: BoxFit.contain),
+                    ),
+                  ),
+                ),
+                Padding(
+                  padding: EdgeInsets.all(10.0),
+                  child: Container(
+                    height: MediaQuery.of(context).size.height,
+                    width: MediaQuery.of(context).size.width * 0.8,
+                    decoration: BoxDecoration(
+                      borderRadius: BorderRadius.circular(30),
+                      color: Colors.white,
+                    ),
+                  ),
+                )
+              ],
+            )));
   }
 }
