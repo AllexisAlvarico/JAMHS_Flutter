c60e03c6f9cefeeac4cad7d2d8da7211bb98de2e
Richard Fleming <c00241212@itcarlow.ie>
2021-03-19 11:19:31 +0000

    Panorama View implemented

--- lib/virtualTourpage.dart
+++ lib/virtualTourpage.dart
@@ -7,6 +7,19 @@ class VirutalToursPage extends StatefulWidget {
 }
 
 class _VirutalToursPageState extends State<VirutalToursPage> {
+  double _lon = 0;
+  double _lat = 0;
+  double _tilt = 0;
+  int _panoId = 0;
+
+  void onViewChanged(longitude, latitude, tilt) {
+    setState(() {
+      _lon = longitude;
+      _lat = latitude;
+      _tilt = tilt;
+    });
+  }
+
   Widget hotspotButton(
       {String? text, IconData? icon, VoidCallback? onPressed}) {
     return Column(
@@ -26,8 +39,12 @@ class _VirutalToursPageState extends State<VirutalToursPage> {
                 padding: EdgeInsets.all(4.0),
                 decoration: BoxDecoration(
                     color: Colors.black38,
-                    borderRadius: BorderRadius.all(Radius.circular(4))),
-                child: Center(child: Text(text)),
+                    borderRadius: BorderRadius.all(Radius.circular(3))),
+                child: Center(
+                    child: Text(
+                  text,
+                  textScaleFactor: 0.2,
+                )),
               )
             : Container(),
       ],
@@ -43,20 +60,157 @@ class _VirutalToursPageState extends State<VirutalToursPage> {
         primarySwatch: Colors.blue,
       ),
       home: Panorama(
+        onViewChanged: onViewChanged,
+        onTap: (longitude, latitude, tilt) =>
+            print('onTap: $longitude, $latitude, $tilt, $_panoId'),
         child: Image.asset('assets/images/DisplayPanoramicOne.jpg'),
         minLatitude: -20.0,
         maxLatitude: 35.0,
-
-        /*hotspots: [
+        hotspots: [
           Hotspot(
             name: "Display 1",
-            latitude: 10.0,
-            longitude: 0.0,
-            width: 40.0,
-            height: 40.0,
-            }
-          )
-        ],*/
+            latitude: 20.0,
+            longitude: 16.0,
+            width: 90,
+            height: 75,
+            widget: hotspotButton(
+                text: "Expand",
+                icon: Icons.zoom_in,
+                onPressed: () => setState(() => _panoId++)),
+          ),
+          Hotspot(
+            name: "Benjamin Display",
+            latitude: 20.0,
+            longitude: 51.0,
+            width: 90,
+            height: 75,
+            widget: hotspotButton(
+                text: "Expand",
+                icon: Icons.zoom_in,
+                onPressed: () => setState(() => _panoId++)),
+          ),
+          Hotspot(
+            name: "Isaac Simon Display",
+            latitude: 20.0,
+            longitude: 65.0,
+            width: 90,
+            height: 75,
+            widget: hotspotButton(
+                text: "Expand",
+                icon: Icons.zoom_in,
+                onPressed: () => setState(() => _panoId++)),
+          ),
+          Hotspot(
+            name: "David Display",
+            latitude: 20.0,
+            longitude: 80.0,
+            width: 90,
+            height: 75,
+            widget: hotspotButton(
+                text: "Expand",
+                icon: Icons.zoom_in,
+                onPressed: () => setState(() => _panoId++)),
+          ),
+          Hotspot(
+            name: "Leopold Display",
+            latitude: 20.0,
+            longitude: 110.0,
+            width: 90,
+            height: 75,
+            widget: hotspotButton(
+                text: "Expand",
+                icon: Icons.zoom_in,
+                onPressed: () => setState(() => _panoId++)),
+          ),
+          Hotspot(
+            name: "Henry Display",
+            latitude: 20.0,
+            longitude: 140.0,
+            width: 90,
+            height: 75,
+            widget: hotspotButton(
+                text: "Expand",
+                icon: Icons.zoom_in,
+                onPressed: () => setState(() => _panoId++)),
+          ),
+          Hotspot(
+            name: "Sam Ben Display",
+            latitude: 30.0,
+            longitude: 172.0,
+            width: 90,
+            height: 75,
+            widget: hotspotButton(
+                text: "Expand",
+                icon: Icons.zoom_in,
+                onPressed: () => setState(() => _panoId++)),
+          ),
+          Hotspot(
+            name: "Sydney Display",
+            latitude: 20.0,
+            longitude: 219.0,
+            width: 90,
+            height: 75,
+            widget: hotspotButton(
+                text: "Expand",
+                icon: Icons.zoom_in,
+                onPressed: () => setState(() => _panoId++)),
+          ),
+          Hotspot(
+            name: "Bill Ben Display",
+            latitude: 20.0,
+            longitude: 248.0,
+            width: 90,
+            height: 75,
+            widget: hotspotButton(
+                text: "Expand",
+                icon: Icons.zoom_in,
+                onPressed: () => setState(() => _panoId++)),
+          ),
+          Hotspot(
+            name: "Ray Isadore Display",
+            latitude: 20.0,
+            longitude: 276.0,
+            width: 90,
+            height: 75,
+            widget: hotspotButton(
+                text: "Expand",
+                icon: Icons.zoom_in,
+                onPressed: () => setState(() => _panoId++)),
+          ),
+          Hotspot(
+            name: "Tibor Ruben Display",
+            latitude: 20.0,
+            longitude: 305.0,
+            width: 90,
+            height: 75,
+            widget: hotspotButton(
+                text: "Expand",
+                icon: Icons.zoom_in,
+                onPressed: () => setState(() => _panoId++)),
+          ),
+          Hotspot(
+            name: "Jack Display",
+            latitude: 20.0,
+            longitude: 327.0,
+            width: 90,
+            height: 75,
+            widget: hotspotButton(
+                text: "Expand",
+                icon: Icons.zoom_in,
+                onPressed: () => setState(() => _panoId++)),
+          ),
+          Hotspot(
+            name: "John Display",
+            latitude: 20.0,
+            longitude: 340.0,
+            width: 90,
+            height: 75,
+            widget: hotspotButton(
+                text: "Expand",
+                icon: Icons.zoom_in,
+                onPressed: () => setState(() => _panoId++)),
+          ),
+        ],
       ),
     );
   }
