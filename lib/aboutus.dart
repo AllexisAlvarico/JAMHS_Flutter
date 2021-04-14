aedf1c33e811ba1eb31117bfa9ac0cdd753df80b
Joshua Dunne <c00241588@itcarlow.ie>
2021-04-14 15:58:11 +0100

    Making some attempts at replacing About Us section with Cards.

--- lib/aboutus.dart
+++ lib/aboutus.dart
@@ -59,105 +59,104 @@ class _AboutUsPageState extends State<AboutUsPage> {
                 ),
               ),
               Padding(
-                padding: EdgeInsets.all(
-                  sy(SizeConfig.edgeINSETS),
-                ),
-                child: Container(
-                  height: sy(1300), // 195 isbottom limit
-                  decoration: BoxDecoration(
-                    borderRadius:
-                        BorderRadius.circular(sy(SizeConfig.borderRADIUS)),
-                    color: Colors.white,
+                  padding: EdgeInsets.all(
+                    sy(SizeConfig.edgeINSETS),
                   ),
-                  child: ListView(
-                    physics: NeverScrollableScrollPhysics(),
-                    children: [
-                      createText("About Us", sy(SizeConfig.edgeINSETS),
-                          SizeConfig.fontHEADERSIZE, FontWeight.bold),
-                      createText(
-                          "The purpose of the Jewish American Military " +
-                              "Historical Society is to preserve Jewish " +
-                              "American military history, interpret the " +
-                              "impact of Jews to military and civil " +
-                              "service, and educating the public to these " +
-                              "contributions.",
-                          sy(SizeConfig.edgeINSETS),
-                          SizeConfig.fontDISCRIPTIONSIZE,
-                          FontWeight.bold),
-                      createImage(
-                          "assets/images/exbibit.png",
-                          sy(SizeConfig.edgeINSETS), // border's radius
-                          EdgeInsets.fromLTRB(
-                              sx(SizeConfig.blockSizeHorizontal * 5),
-                              sy(SizeConfig.blockSizeVertical * 1),
-                              sx(SizeConfig.blockSizeHorizontal * 5),
-                              sy(SizeConfig.blockSizeVertical * 3)),
-                          null),
-                      createText("Partners", sy(SizeConfig.edgeINSETS),
-                          SizeConfig.fontHEADERSIZE, FontWeight.bold),
-                      createTriple(
-                          'assets/images/calguard.png',
-                          'assets/images/vshp.png',
-                          'assets/images/siteline.png',
-                          launchCalguard,
-                          launchVSHP,
-                          launchSiteline,
-                          EdgeInsets.fromLTRB(
-                              sx(SizeConfig.blockSizeHorizontal * 5),
-                              sy(SizeConfig.blockSizeVertical * 1),
-                              sx(SizeConfig.blockSizeHorizontal * 5),
-                              sy(SizeConfig.blockSizeVertical * 3))),
-                      createText(
-                          "We've partnered with the California State Military Museum, " +
-                              "Virtual Socity Historical Preservation(VSHP) and Siteline Productions",
-                          sy(SizeConfig.edgeINSETS),
-                          SizeConfig.fontDISCRIPTIONSIZE,
-                          FontWeight.bold),
-                      createText("Donate Now!", sy(SizeConfig.edgeINSETS),
-                          SizeConfig.fontHEADERSIZE, FontWeight.bold),
-                      createText(
-                          "to the Jewish American Military Historical Society!",
-                          sy(SizeConfig.edgeINSETS),
-                          SizeConfig.fontDISCRIPTIONSIZE,
-                          FontWeight.bold),
-                      createImage(
-                          "assets/images/paypal.png",
-                          sy(SizeConfig.borderRADIUS),
-                          EdgeInsets.fromLTRB(
-                              sx(SizeConfig.blockSizeHorizontal * 5),
-                              sy(SizeConfig.blockSizeVertical * 1),
-                              sx(SizeConfig.blockSizeHorizontal * 5),
-                              sy(SizeConfig.blockSizeVertical * 3)),
-                          launchPaypal),
-                      createText("Social Media", sy(SizeConfig.edgeINSETS),
-                          SizeConfig.fontHEADERSIZE, FontWeight.bold),
-                      createTriple(
-                          "assets/images/facebook.png",
-                          "assets/images/instagram.png",
-                          "assets/images/twitter.png",
-                          launchFacebook,
-                          launchInsta,
-                          launchTwitter,
-                          EdgeInsets.fromLTRB(
-                              sx(SizeConfig.blockSizeHorizontal * 5),
-                              sy(SizeConfig.blockSizeVertical * 1),
-                              sx(SizeConfig.blockSizeHorizontal * 5),
-                              sy(SizeConfig.blockSizeVertical * 1))),
-                      createText("Visit our Website", SizeConfig.edgeINSETS,
-                          SizeConfig.fontHEADERSIZE, FontWeight.bold),
-                      createImage(
-                          "assets/logo/jamhs.jpg",
-                          SizeConfig.edgeINSETS, // border's radius
-                          EdgeInsets.fromLTRB(
-                              SizeConfig.blockSizeHorizontal * 30,
-                              0,
-                              SizeConfig.blockSizeHorizontal * 30,
-                              0),
-                          launchJAHMS),
-                    ],
-                  ),
-                ),
-              )
+                  child: Card(
+                      child: Container(
+                          height: sy(SizeConfig.screenHeight * 1.9),
+                          child: ListView(
+                            physics: NeverScrollableScrollPhysics(),
+                            children: [
+                              createText("About Us", sy(SizeConfig.edgeINSETS),
+                                  SizeConfig.fontHEADERSIZE, FontWeight.bold),
+                              createText(
+                                  "The purpose of the Jewish American Military " +
+                                      "Historical Society is to preserve Jewish " +
+                                      "American military history, interpret the " +
+                                      "impact of Jews to military and civil " +
+                                      "service, and educating the public to these " +
+                                      "contributions.",
+                                  sy(SizeConfig.edgeINSETS),
+                                  SizeConfig.fontDISCRIPTIONSIZE,
+                                  FontWeight.bold),
+                              createImage(
+                                  "assets/images/exbibit.png",
+                                  sy(SizeConfig.edgeINSETS), // border's radius
+                                  EdgeInsets.fromLTRB(
+                                      sx(SizeConfig.blockSizeHorizontal * 5),
+                                      sy(SizeConfig.blockSizeVertical * 3),
+                                      sx(SizeConfig.blockSizeHorizontal * 5),
+                                      sy(SizeConfig.blockSizeVertical * 3)),
+                                  null),
+                              createText("Partners", sy(SizeConfig.edgeINSETS),
+                                  SizeConfig.fontHEADERSIZE, FontWeight.bold),
+                              createTriple(
+                                  'assets/images/calguard.png',
+                                  'assets/images/vshp.png',
+                                  'assets/images/siteline.png',
+                                  launchCalguard,
+                                  launchVSHP,
+                                  launchSiteline,
+                                  EdgeInsets.fromLTRB(
+                                      sx(SizeConfig.blockSizeHorizontal * 5),
+                                      sy(SizeConfig.blockSizeVertical * 3),
+                                      sx(SizeConfig.blockSizeHorizontal * 5),
+                                      sy(SizeConfig.blockSizeVertical * 3))),
+                              createText(
+                                  "We've partnered with the California State Military Museum, " +
+                                      "Virtual Socity Historical Preservation(VSHP) and Siteline Productions",
+                                  sy(SizeConfig.edgeINSETS),
+                                  SizeConfig.fontDISCRIPTIONSIZE,
+                                  FontWeight.bold),
+                              createText(
+                                  "Donate Now!",
+                                  sy(SizeConfig.edgeINSETS),
+                                  SizeConfig.fontHEADERSIZE,
+                                  FontWeight.bold),
+                              createText(
+                                  "to the Jewish American Military Historical Society!",
+                                  sy(SizeConfig.edgeINSETS),
+                                  SizeConfig.fontDISCRIPTIONSIZE,
+                                  FontWeight.bold),
+                              createImage(
+                                  "assets/images/paypal.png",
+                                  sy(SizeConfig.borderRADIUS),
+                                  EdgeInsets.fromLTRB(
+                                      sx(SizeConfig.blockSizeHorizontal * 5),
+                                      sy(SizeConfig.blockSizeVertical * 3),
+                                      sx(SizeConfig.blockSizeHorizontal * 5),
+                                      sy(SizeConfig.blockSizeVertical * 3)),
+                                  launchPaypal),
+                              createText(
+                                  "Social Media",
+                                  sy(SizeConfig.edgeINSETS),
+                                  SizeConfig.fontHEADERSIZE,
+                                  FontWeight.bold),
+                              createTriple(
+                                  "assets/images/facebook.png",
+                                  "assets/images/instagram.png",
+                                  "assets/images/twitter.png",
+                                  launchFacebook,
+                                  launchInsta,
+                                  launchTwitter,
+                                  EdgeInsets.fromLTRB(
+                                      sx(SizeConfig.blockSizeHorizontal * 5),
+                                      sy(SizeConfig.blockSizeVertical * 3),
+                                      sx(SizeConfig.blockSizeHorizontal * 5),
+                                      sy(SizeConfig.blockSizeVertical * 3))),
+                              createText(
+                                  "Visit our Website",
+                                  sy(SizeConfig.edgeINSETS),
+                                  SizeConfig.fontHEADERSIZE,
+                                  FontWeight.bold),
+                              createImage(
+                                  "assets/logo/jamhs.jpg",
+                                  sy(SizeConfig.edgeINSETS), // border's radius
+                                  EdgeInsets.fromLTRB(sx(140), 0, sx(140), 0),
+                                  launchJAHMS),
+                            ],
+                          ))))
             ],
           ),
         );
